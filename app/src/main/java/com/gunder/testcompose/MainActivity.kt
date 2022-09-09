5e55c701c45ab20bf35e448f19474695ec7e293c
itselaras <gunadermawan62@gmail.com>
2022-09-09 23:31:08 +0700

    try create some fun with compose

--- app/src/main/java/com/gunder/testcompose/MainActivity.kt
+++ app/src/main/java/com/gunder/testcompose/MainActivity.kt
@@ -3,12 +3,17 @@ package com.gunder.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.gunder.testcompose.ui.theme.TestComposeTheme
 
@@ -18,8 +23,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = MaterialTheme.colors.background) {
+                    jetpackCompose()
                 }
             }
         }
@@ -27,14 +33,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun jetpackCompose() {
+    Card {
+        var expand by remember { mutableStateOf(false) }
+        Column(Modifier.clickable { expand = !expand }) {
+            androidx.compose.foundation.Image(painterResource(R.drawable.ic_compose),
+                contentDescription = "welcome to jetpack compose")
+            AnimatedVisibility(expand) {
+                Text(text = "welcome to Jetpack compose",
+                    style = MaterialTheme.typography.h3,
+                    textAlign = TextAlign.Center)
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
-        Greeting("Compoese")
+        jetpackCompose()
     }
 }
